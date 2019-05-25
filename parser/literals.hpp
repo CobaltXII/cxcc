23fe8336e4aaad170fcf4c635d36ccf2cda195cb
CobaltXII <sidatt64@gmail.com>
2019-05-25 18:37:23 -0400

    Added literals (from cxci)

---
+++ parser/literals.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+// An integer literal.
+typedef std::string integer_t;
+
+// A string literal.
+typedef std::string string_t;
+
+// A character literal.
+typedef std::string character_t;
