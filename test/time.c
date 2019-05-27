2ee6e980f43c82ee28429057b9edfe6bf73cd7d7
CobaltXII <sidatt64@gmail.com>
2019-05-27 16:10:54 -0400

    Added libc time example

---
+++ test/time.c
@@ -0,0 +1,43 @@
+int puts(int* str) {
+	while (*str) {
+		putchar(*str);
+		str = str + 1;
+	}
+	return 0;
+}
+
+int puti(int n) {
+	if (n < 0) {
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10) {
+		puti(n / 10);
+	}
+	return putchar(n % 10 + '0');
+}
+
+int main() {
+	putchar('\n');
+	int seconds = time(0);
+	int minutes = seconds / 60;
+	int hours = minutes / 60;
+	int days = hours / 24;
+	int years = days / 365;
+	puts("Seconds since Epoch: ");
+	puti(seconds);
+	putchar('\n');
+	puts("Minutes since Epoch: ");
+	puti(minutes);
+	putchar('\n');
+	puts("Hours since Epoch: ");
+	puti(hours);
+	putchar('\n');
+	puts("Days since Epoch: ");
+	puti(days);
+	putchar('\n');
+	puts("Years since Epoch: ");
+	puti(years);
+	putchar('\n');
+	return 0;
+}
